e2bd5dd85e1365ad5dd5fdad2e75fd05f59a18ed
GC-ZF <zhang15835765326@gmail.com>
2022-12-24 21:50:01 +0800

    add deployment requests>=2.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
 
 setup(
     name='nonebot_plugin_firexN',      #这里是pip项目发布的名称
-    version='0.1.6',  #版本号，数值大的会优先被pip
+    version='0.1.7',  #版本号，数值大的会优先被pip
     keywords = ["pip", "nonebot2", "nonebot", "fire", "nonebot_plugin"],
     description = "nonebot plugin firexN",
     long_description = long_description,
@@ -31,9 +31,9 @@ setup(
     # install_requires = ["numpy"]          #这个项目需要的第三方库
 
     install_requires=[
-        'nonebot-plugin-apscheduler>=0.1.3',
-        'nonebot2>=2.0.0-beta.2',
-        'nonebot-adapter-onebot>=2.0.0b1',
-        'nonebot-adapter-onebot',
+        'nonebot-plugin-apscheduler>=0.2.0',
+        'nonebot2>=2.0.0-beta.4',
+        'nonebot-adapter-onebot>=2.0.0-beta.1',
+        'requests>=2.28.1'
     ]
 )
